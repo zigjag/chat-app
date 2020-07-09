b3452d6c51f74c7bac621568507f7a121dbe4f1f
Joseph Kligel <jkligel@gmail.com>
2020-07-09 10:38:47 -0500

    gitpy to your rescue

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const http = require('http');
+const socketio = require('socket.io')
+
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
 
 app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')));
-
 app.set('views', path.join(__dirname, '../templates/views'))
 hbs.registerPartials(path.join(__dirname, '../templates/partials'))
 
+io.on('connection', () => {
+	console.log('New websocket connection')
+})
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server started on ${PORT}`);
 })
 
